771ba9922224dcb299d724bf330606b9dcd89073
niketica <commerce.alexander.n@gmail.com>
2026-07-24 10:52:00 +0200

    Minor refactor

---
+++ src/engine/adapter/systems/input/InputSystem.cpp
@@ -0,0 +1,29 @@
+#include "engine/adapter/systems/input/InputSystem.h"
+
+namespace niketica::systems
+{
+
+    void InputSystem::input()
+    {
+        auto viewInput = registry->view<niketica::component::InputComponent>();
+        auto& input = viewInput.get<niketica::component::InputComponent>(viewInput.front());
+        auto viewWindow = registry->view<niketica::component::Window>();
+        const auto& window = viewWindow.get<niketica::component::Window>(viewWindow.front());
+
+        auto* state = engineServices->getInputContext()->getInputState();
+        input.mousePos = { state->mousePos.x, window.height - state->mousePos.y };
+        input.mouseDelta = state->mouseDelta;
+        input.scrollDelta = state->scrollDelta;
+
+        mapBindings();
+
+        for (const auto& [action, binding] : mapBindings())
+        {
+            const auto& key = binding.key;
+            const auto& keyState = state->key(key);
+            input.actions[action] = keyState;
+        }
+        engineServices->getInputContext()->clearState();
+    }
+    
+}
--- src/engine/adapter/systems/input/InputSystem.h
+++ src/engine/adapter/systems/input/InputSystem.h
@@ -15,31 +15,8 @@ namespace niketica::systems
         InputSystem(entt::registry* registry, niketica::engine::EngineServices* engineServices)
             : ISystem(registry, engineServices) {}
 
-        void input() override
-        {
-            auto viewInput = registry->view<niketica::component::InputComponent>();
-            auto& input = viewInput.get<niketica::component::InputComponent>(viewInput.front());
-            auto viewWindow = registry->view<niketica::component::Window>();
-            const auto& window = viewWindow.get<niketica::component::Window>(viewWindow.front());
-
-            auto* state = engineServices->getInputContext()->getInputState();
-            input.mousePos = { state->mousePos.x, window.height - state->mousePos.y };
-            input.mouseDelta = state->mouseDelta;
-            input.scrollDelta = state->scrollDelta;
-
-            mapBindings();
-
-            for (const auto& [action, binding] : mapBindings())
-            {
-                const auto& key = binding.key;
-                const auto& keyState = state->key(key);
-                input.actions[action] = keyState;
-            }
-            engineServices->getInputContext()->clearState();
-        }
-
+        void input();
         void update(float dt) override {}
-
         void render() override {}
 
     private:
